90b9f704492d642d451d02742f404bc21f83fed2
hyeongdong <khd941018@gmail.com>
2022-02-05 16:37:55 +0900

    book solution

--- 이것이코딩테스트다/4_2_시각.py
+++ 이것이코딩테스트다/4_2_시각.py
@@ -1,24 +1,23 @@
-# 00시00분00초부터 n시 59분 59초까지 3이 하나라도 포함되는 경우의 수 구하기
-# 매시간마다 3이 들어가는 경우의 수는 같다.
-# 3시엔 모든 분,초가 경우의 수에 포함된다.
-# 시간>분>초 순으로 for문을 돌려 구해본다..
+# 책의 풀이
+# 완전 탐색 - 가능한 모든 경우의 수 검사
+# 데이터크기가 매우 클 경우 다른 방법 고려해보자
 
-n = int(input())
+h = int(input())
+count = 0
 
-number_of_case = 0
-minutes = 60
-seconds = 60
-num_include_3 = [3, 13, 23, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 43, 53]
+for hour in range(h+1):
+    for min in range(60):
+        for sec in range(60):
+            if '3' in str(hour) + str(min) + str(sec):
+                count += 1
 
-for hour in range(n+1):
-    if hour == 3:
-        number_of_case += minutes * seconds
-        continue
 
-    for min in range(minutes):
-        if min in num_include_3:
-            number_of_case += seconds
-        else:
-            number_of_case += len(num_include_3)
+print(count)
 
-print(number_of_case)
+# 책의 풀이는 로직이 정말 깔끔한 것 같다..
+# 나도 비슷한 방식으로 접근하였지만, 좀 복잡하게 구현한 것 같다.
+# 3중 for문 돌리는 것이 비효율적이라 생각했다.
+# 또 str을 활용하는 방법을 생각하였지만, 마땅한 방법이 떠오르지 않아 다른 방식으로 구현하였었다.
+# 전체 경우의 수가 얼마나 되는지 확인해보고 구현을 하도록 하자.
+
+# if '3' in str(hour) + str(min) + str(sec) 요렇게도 활용할 수 있구나
