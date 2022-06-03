0e5b9f785460cd05e9128d6710c0e5e97573e027
hyeongdong <87406219+dancewithmebaby@users.noreply.github.com>
2022-06-03 17:22:22 +0900

    자가진단
    reduce와 객체를 활용한 방법

--- boostcamp.js
+++ boostcamp.js
@@ -1,19 +1,21 @@
 function overlap(arr){
     const answer = []
-    const arrToSet = new Set(arr)
-    console.log(arrToSet)
-    for(let num of arrToSet){
-        let count = 0
-        for(let i=0;i<arr.length;i++){
-            if(num===arr[i]){
-                count++
-            }
+    
+    //obj : 숫자n의 개수를 저장하는 객체. 숫자 n을 key, n의 개수를 value
+    
+    const result= arr.reduce((obj,num)=>{
+        obj[num] = (obj[num]||0) + 1
+        return obj
+    },{})
+    
+    for(let num in result ){
+        if(result[num]>1){
+            answer.push(result[num])
         }
-        if(count===1) continue
-        if(count>1){
-            answer.push(count)
-        }
-
     }
-    return answer.length? answer : -1
+    return answer.length ? answer : -1
 }
+
+
+// reduce와 객체를 사용하니 코드가 훨씬 깔끔해졌다.
+// 반복횟수도 줄어들었다. 
