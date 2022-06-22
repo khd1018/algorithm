1093b9368fe34dc1fa9dbe3be2b6b07c18fd33b3
hyeongdong <87406219+dancewithmebaby@users.noreply.github.com>
2022-06-22 16:27:48 +0900

    Update 신고결과받기.js

--- 프로그래머스/신고결과받기.js
+++ 프로그래머스/신고결과받기.js
@@ -1,45 +1,32 @@
 function solution(id_list, report, k) {
     
-    // 각 유저별로 처리 결과 메일을 받은 횟수를 배열에 담아 return
+    const excludedReport = [...new Set(report)].map(str=> str.split(" "))
     
-    const reportInfoById = {} // 각 유저별 신고한 유저를 담는 객체
-    const reportCountById = {} // 각 유저별 신고당한 횟수를 담는 객체
+    const reportCountById = {} // 각 유저별 신고당한 횟수를 저장하는 객체
+    const responseCountById = {} // 각 유저별로 메일을 받는 횟수 저장하는 객체
     
-    // 객체들을 각각 빈배열과 0으로 초기화
-    id_list.forEach(id=>{
-        reportInfoById[id] = [] 
-        reportCountById[id] = 0
+    // 각 유저별 신고당한 횟수를 reportCountById에 저장
+    excludedReport.forEach((reportIdList)=>{
+        const reportedId = reportIdList[1]
+        reportCountById[reportedId] = reportCountById[reportedId] + 1 || 1
     })
     
-    // 문자열로 구성된 report을 순회하며 신고한 아이디와 신고당한 아이디 분리하여 각 객체에 값을 할당
-    report.forEach((str)=>{
-        const reportId = str.split(" ")[0]
-        const reportedId = str.split(" ")[1]
-        
-        // 한 유저가 같은 유저를 중복 신고한 경우 제외
-        if(!reportInfoById[reportId].includes(reportedId)){
-            reportInfoById[reportId].push(reportedId)
-            reportCountById[reportedId] += 1
+    // 각 유저가 신고한 유저들 중 정지당한 유저의 숫자를 responseCountById에 저장
+    excludedReport.forEach((reportIdList)=>{
+        const reportId = reportIdList[0]
+        const reportedId = reportIdList[1]
+        if( reportCountById[reportedId]>=k){
+            responseCountById[reportId] = responseCountById[reportId] + 1 || 1
         }
     })
-    // 신고당한 회수가 k이상인 유저(정지당한 유저)를 배열로 저장
-    const bannedIdList= Object.keys(reportCountById).filter((id)=>{
-        return reportCountById[id] < k ? false : true 
+    
+    // 유저별로 신고한 유저 중 정지당한 유저의 숫자를 반환, 없으면 0을 반환
+    const answer = id_list.map((id)=>{
+        return responseCountById[id] || 0
     })
-    
-    // 각 유저가 신고한 유저 중 정지당한 유저의 숫자를 배열로 반환
-    const answer = id_list.map((user)=>{
-        let count = 0
-        const idListReportedByUser= reportInfoById[user]
-        bannedIdList.forEach((bannedId)=>{
-            if(idListReportedByUser.includes(bannedId)) count++
-        })
-        
-        return count
-        
-    })
-    
-    
+
     return answer
 
 }
+
+
