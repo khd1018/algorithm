b943cfbf91e72d0a7e6b0ab1d68882c24d309973
hyeongdong <87406219+dancewithmebaby@users.noreply.github.com>
2022-09-08 21:10:25 +0900

    재귀를활용한코드

--- 프로그래머스/124나라의숫자.js
+++ 프로그래머스/124나라의숫자.js
@@ -1,30 +1,31 @@
-// 제한시간초과
-// 당연히 안될것 같았지만 한번 구현해봤다.
-
 function solution(n) {
     
-    let former = 0
+    const converted = divide(n).split("")
     
-    for(let i=1;i<=n;i++){
-        
-        let decimal= former + 1
-        
-        while(true){
-        
-            const isContained124 = String(decimal).split("").every((value)=>{
-                return value==='1' || value==='2' || value === '4' ? true : false
-            })
+    return converted.map((num)=>num==='3'? 4 : num ).join("")
+    
+    
+}
 
-            if(!isContained124){
-                decimal++
-                continue
-            }
-            
-            former = decimal
-            break
+// 삼진법을 조금 변형하면 되는 문제
+// 규칙을 이해하기 힘들었다...
+// 
+
+const divide = (n)=>{
     
-        }
+    
+    let remainder = n%3
+    let quotient =  parseInt(n/3)
+    
+    if(n%3===0){
+        quotient -= 1
+        remainder = n - (3*quotient)
     }
-
-    return String(former)
+    
+        
+    if(parseInt(n/3)<3){
+        return quotient===0? `${remainder}` : `${quotient}${remainder}`
+    }
+        
+    return `${divide(quotient)}${remainder}`
 }
