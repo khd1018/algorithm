e55efce1cbf85e513c156b2e17deb5c0db968148
hyeongdong <87406219+dancewithmebaby@users.noreply.github.com>
2022-09-08 22:45:48 +0900

    for문을사용하여문제해결

--- 프로그래머스/피보나치수.js
+++ 프로그래머스/피보나치수.js
@@ -1,17 +1,12 @@
-// 간단히 피보나치 수열을 구현하는 문제인줄 알았는데 시간초과 및 런타임에러가 발생하였다.
-// 바로 숫자 범위를 넘어서기 때문이었다.. 
-
+// https://school.programmers.co.kr/questions/11991를 참고하여 재귀 대신 for문을 사용하여 피보나치 수열을 구했고, 매 값마다 1234567을 나눠줬다.
 
 function solution(n) {
-    return f(n)  % 1234567
-}
-
-const f = (n)=> {
     
-    if(n===0 || n===1){
-        return n
-    }
+    const fiboArray = Array(n).fill(0)
     
-    return solution(n-1) + solution(n-2)
-        
+    fiboArray[1] = 1
+    
+    for(let i=2; i<=n; i++) fiboArray[i] = (fiboArray[i-1] + fiboArray[i-2]) % 1234567
+    
+    return fiboArray[n]
 }
