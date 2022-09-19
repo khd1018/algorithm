64a2d52032fbe9945e08d32d9f6b4d08cf6aa364
hyeongdong <87406219+khd1018@users.noreply.github.com>
2022-09-19 17:33:30 +0900

    secondSolve

--- 프로그래머스/예상대진표.js
+++ 프로그래머스/예상대진표.js
@@ -31,3 +31,25 @@ function solution(n,a,b)
 
 // 통과는 했으나 27번 케이스부터 시간효율성이 많이 떨어짐. 그럴거 같았음..
 // 분명 다른 간단한 방법이 있을 것이라 생각함
+
+// 다른 방법으로 해결!!
+// 참가자의 숫자가 짝수로 주어지는 것에서 착안해 a,b를 나누고 올림하면 다음 라운드  a,b의 순서라는 것을 알게됨.
+
+function solution(n,a,b)
+{
+    let matchCount = 1
+    let [max, min] = [Math.max(a,b),Math.min(a,b)]
+    
+    while(true){
+        
+        if(min%2!==0 &&min+1===max) break
+        
+        min = Math.ceil(min/2)
+        max =  Math.ceil(max/2)
+        matchCount ++
+        
+    }
+    
+    return matchCount
+    
+}
