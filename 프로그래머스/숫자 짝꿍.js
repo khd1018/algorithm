0832533a1a876ba20b834b95fc5c8d696e95a37c
hyeongdong <87406219+khd1018@users.noreply.github.com>
2023-05-18 21:26:30 +0900

    solve

--- 프로그래머스/숫자 짝꿍.js
+++ 프로그래머스/숫자 짝꿍.js
@@ -1,54 +1,58 @@
-X, Y의 짝꿍이 0으로만 구성되어 있다면, 짝꿍은 0
+// 정수 x,y의 각 자리 숫자 개수를 count
+// x,y에서 공통으로 나타나는 숫자들의 쌍의 개수를 count
+// 공통으로 나타나는 숫자를 모두 나열
 
-- x,y에서 각 정수가 몇 개씩 있는지 count하는 기능
-- 공통 정수를 찾는 기능
-- 공통 정수를 내림차순으로 정렬하는 기능
-
-*/
-
-// 객체 배열 복사, airbnb 컨벤션 배열? 여러값 매개변수 관련, 프로퍼티명 숫자 가능?
-// 
 
 function solution(X, Y) {
+    const xObj = countDigit(X)
+    const yObj = countDigit(Y)
+
+    const numberOfPairs = countPairOfSameNumber(yObj,xObj)
+    const couple = getAllSameNumbers(numberOfPairs).sort((a,b)=>b-a)
+
+    return getResult(couple)
+}
+
+const countPairOfSameNumber = (xObj,yObj)=>{
+    const numberOfPairs = {}
     
-    const commonNumbers = []
-    let shorterInteger= X
-    let longerInteger = Y
+    Object.entries(xObj).forEach(([xNumber,xCount])=>{
+        if(yObj[xNumber]){
+            numberOfPairs[xNumber] = Math.min(xCount,yObj[xNumber])
+        }
+    })
     
-    if(X.length > Y.length){
-        shorterInteger = Y
-        longerInteger = X
-        
-    }
+    return numberOfPairs
+}
+
+const getResult = (sameNumbers)=>{
+    if(!sameNumbers.length)
+        return "-1"
     
-    for(const shorterNum of shorterInteger){
-        const indexOfSameNumber= getIndexOfSameNumber(shorterNum,longerInteger)
-        if(indexOfSameNumber === -1){
+    if(sameNumbers[0] === "0")
+        return "0"
+    
+    return sameNumbers.join('')
+}
+
+const countDigit = (integer)=>{
+    const numberOfDigits = {}
+    
+    for(const number of integer){
+        if(!numberOfDigits[number]){
+            numberOfDigits[number] = 1
             continue
         }
-        commonNumbers.push(shorterNum)
-        longerInteger = longerInteger.slice(0,indexOfSameNumber) + longerInteger.slice(indexOfSameNumber+1,)
-        
+        numberOfDigits[number] ++
     }
     
-    if(!commonNumbers.length) return "-1"
-    if(commonNumbers.every(number=>number==="0")) return "0"
-    
-    return commonNumbers.sort((a,b)=>b-a).join("")
-    
+    return numberOfDigits
 }
-                            
-const getIndexOfSameNumber = (shorterNum,longerInteger)=>{
-    let indexOfSameNumber =  -1
-    
-    longerInteger.split("").some((longerNum,i)=>{
-            if(shorterNum === longerNum){
-                indexOfSameNumber = i
-                return true
-            }
-            return false
-        })
-    
-    return indexOfSameNumber
-} 
+
+const getAllSameNumbers = (concatObj)=>{
+    return Object.entries(concatObj).reduce((allSameNumbers,[sameNumber,count])=>{
+        allSameNumbers += sameNumber.repeat(count)
+        return allSameNumbers
+    },"").split("")
+}
 
