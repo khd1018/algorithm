f2c1da7bc9d47c017def12c0334aa778b1dccea4
hyeongdong <87406219+khd1018@users.noreply.github.com>
2023-05-22 20:51:48 +0900

    refactor : 점수의 합을 reduce를 사용해서 구현

--- 프로그래머스/추억점수.js
+++ 프로그래머스/추억점수.js
@@ -1,16 +1,16 @@
 function solution(name, yearning, photo) {
     const scoreByName = {}
+    
     name.forEach((name,index)=>{
         scoreByName[name] = yearning[index]
     })
     
     return photo.map((people)=>{
         
-        let totalScoreOfPic = 0
-        
-        people.forEach(person=>{
-            totalScoreOfPic += scoreByName[person] ? scoreByName[person] : 0
-        })
+        const totalScoreOfPic = people.reduce((scoreSum,person)=>{
+            scoreSum += scoreByName[person] ? scoreByName[person] : 0
+            return scoreSum
+        },0)
         
         return totalScoreOfPic
     })
