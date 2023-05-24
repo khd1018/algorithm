f638a4d6f5fdf253ee6ba0a5b8a3a54b81050f70
hyeongdong <87406219+khd1018@users.noreply.github.com>
2023-05-25 01:44:04 +0900

    첫번째코드

---
+++ 프로그래머스/옹알이(다시풀기).js
@@ -0,0 +1,63 @@
+
+/*
+1. 가능한 발음인지 확인
+    - 해당 단어가 가능한 발음들의 조합이어야 함 
+2. 가능한 발음이 연속되는지 확인
+*/
+
+
+function solution(babbling) {
+    
+    const possiblePronuns = ["aya", "ye", "woo", "ma"]
+    let count = 0
+    
+    babbling.forEach(babble=>{
+
+        if(!checkSpeakable(babble,possiblePronuns)){
+            return
+        }
+        
+        if(!isContiuos(babble,possiblePronuns)){
+            return
+        }
+        
+        count++
+    })
+    
+    return count
+}
+
+
+const checkSpeakable = (babble,possiblePronuns)=>{
+    
+    if(!babble.length){
+        return true
+    }
+    
+    for(let i=0; i<possiblePronuns.length; i++){
+        const lasts = babble.split(possiblePronuns[i])
+        
+        if(lasts[0]!==babble){
+            const rest = possiblePronuns.slice(i+1)
+            const isSpeakable = lasts.every((last)=>checkSpeakable(last,rest))
+            
+            return isSpeakable ? true : false
+        }
+    }
+    
+    return false
+}
+
+const isContiuos = (babble,possiblePronuns)=>{
+    
+    return possiblePronuns.every((possiblePronun)=>{
+        const regex = new RegExp(`(${possiblePronun})`+"\\1+","g")
+        
+        if(regex.test(babble)){
+            return false
+        }
+        
+        return true
+    })
+    
+}
