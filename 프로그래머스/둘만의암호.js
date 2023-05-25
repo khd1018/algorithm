55fe0efd6a836edb4cb87c2f6d8bf60cb4d9d544
hyeongdong <87406219+khd1018@users.noreply.github.com>
2023-05-25 22:50:36 +0900

    첫번째코드

---
+++ 프로그래머스/둘만의암호.js
@@ -0,0 +1,38 @@
+function solution(s, skip, index) {
+    const alphabets = [...getAllAlphabets(skip)]
+    const targetAlphabets = s.split("")
+    
+    return targetAlphabets.map((targetAlphabet)=>{
+        const targetIndex= alphabets.indexOf(targetAlphabet)
+        const indexOfconverted = targetIndex + index
+        
+        if(indexOfconverted<alphabets.length){
+            return alphabets[indexOfconverted]
+        }
+        
+         return alphabets[indexOfconverted%alphabets.length]
+        
+    }).join("")
+}
+
+const getAllAlphabets = (skip)=>{
+    const alphabets = []
+    
+    for(let i=0;i<26;i++){
+        const alphabet = String.fromCharCode(97+i)
+        
+        if(isSkipAlpha(alphabet,skip)){
+            continue
+        }
+        
+        alphabets.push(alphabet)
+    }
+    
+    return alphabets
+}
+
+
+const isSkipAlpha = (alpha,skip)=>{
+    
+    return skip.includes(alpha) ? true : false
+}
